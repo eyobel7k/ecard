2af697ad73e673583395de68872d124b8ab3159c
Eyobel Legese <eyobel7k@gmail.com>
2021-12-13 11:20:21 -0800

    chore(App.js): update spelling in useState initial value

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import GreetingCard from "./Components/GreetingCard";
 import { useState } from "react";
 
 function App() {
-  const [greeting, setGreeting] = useState("Dear Mom");
-  const [body, setBody] = useState("Have a comforting and Joyous holida");
-  const [closing, setClosing] = useState("Love your son Eyob");
+  const [greeting, setGreeting] = useState("Dear Mom,");
+  const [body, setBody] = useState("Have a comforting and Joyous holiday");
+  const [closing, setClosing] = useState("Love your son, Eyob");
 
   return <GreetingCard 
           greeting={greeting} 
