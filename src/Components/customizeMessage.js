cad1a7e3370c6b499cd65d517efb2f12d2c5da87
Eyobel Legese <eyobel7k@gmail.com>
2021-12-14 09:54:09 -0800

    feat(instructions): add instructions to resned message

--- src/Components/customizeMessage.js
+++ src/Components/customizeMessage.js
@@ -1,5 +1,11 @@
+import Christmas from "../images/Christmas.jpg";
+
 export default function CustomizeMessage(){
   return(
-    <p>Customize Message</p>
+    <div className="instructions">
+    <img src={Christmas} className="thumb" alt="Christmas images" />
+    
+    <p><strong>How to Reshare:</strong><br />Write your own custom message below</p>
+    </div>
   )
 }
