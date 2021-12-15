01347cb5ed866a54f85f42382193dd38f8bb235d
Eyobel Legese <eyobel7k@gmail.com>
2021-12-14 18:30:46 -0800

    git commit -m "feat(App.js): add useState for preview and setPreview"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import "./App.css";
 import GreetingCard from "./Components/GreetingCard";
-import { useState } from "react";
 import CustomizeMessage from "./Components/customizeMessage";
+import { useState } from "react";
 
 function App() {
+  const[email, setEmail]=useState('');
   const [greeting, setGreeting] = useState("Dear Mom,");
   const [body, setBody] = useState("Have a comforting and Joyous holiday");
   const [closing, setClosing] = useState("Love your son, Eyob");
   const [customize, setCustomize]=useState(false)
+  const [preview, setPreview]=useState(false)
+
 
   return(
     <>
     {customize
      
-      ? <CustomizeMessage />
+      ? <CustomizeMessage 
+      email={email}
+      setEmail={setEmail}
+      greeting={greeting}
+      setGreeting={setGreeting}
+      body={body}
+      setBody={setBody}
+      closing={closing}
+      setClosing={setClosing}
+      setPreview={setPreview}
+      setCustomize={setCustomize}
+      
+      
+      />
       : <GreetingCard 
+      email={email}
       greeting={greeting} 
       body={body} 
       closing={closing} 
       setCustomize={setCustomize}
+      preview={preview}
+      setPreview={setPreview}
+
       />
     
     }
