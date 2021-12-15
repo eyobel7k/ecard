f29b0e1c56a1ff397013e7282a4b8e8349a46fc1
Eyobel Legese <eyobel7k@gmail.com>
2021-12-14 18:26:29 -0800

    feat(greeting page): add send function

--- src/Components/GreetingCard.js
+++ src/Components/GreetingCard.js
@@ -1,7 +1,41 @@
 import Christmas from "../images/Christmas.jpg";
 import "../App.css";
 
-function GreetingCard({ greeting, body, closing, setCustomize}) {
+function GreetingCard({
+  greeting,
+  body,
+  closing,
+  setCustomize,
+  preview,
+  setPreview,
+  email,
+}) {
+  const continueEditing = () => {
+    setCustomize(true);
+    setPreview(false);
+  };
+  const sendEcard = () => {
+    {
+      let mailto = `mailto:${email}`;
+      mailto += "?subject=E-Card";
+      mailto += "&body=You have received an e-greeting card.";
+      mailto += "%0D%0A%0D%0A";
+      mailto += "Go To Greeting Card";
+      mailto += "%0D%0A";
+
+      let url = "https://eyobel7k.github.io/ecard";
+      url += `?greeting=${encodeURIComponent(greeting)}`;
+      url += `&body=${encodeURIComponent(body)}`;
+      url += `&closing=${encodeURIComponent(closing)}`;
+
+      mailto += encodeURIComponent(url);
+
+      console.log(mailto);
+
+      window.open(mailto);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +43,16 @@ function GreetingCard({ greeting, body, closing, setCustomize}) {
         <p>{greeting}</p>
         <p>{body}</p>
         <p>{closing}</p>
-        <button 
-        className="App-link"
-        onClick={()=>setCustomize(true)}>Reshare This Card</button>
+        {preview ? (
+          <div>
+            <button onClick={continueEditing}>ContinueEditing</button>
+            <button onClick={sendEcard}>Send</button>
+          </div>
+        ) : (
+          <button className="App-link" onClick={() => setCustomize(true)}>
+            Reshare This Card
+          </button>
+        )}
       </header>
     </div>
   );
--- src/Components/customizeMessage.js
+++ src/Components/customizeMessage.js
@@ -1,11 +1,93 @@
 import Christmas from "../images/Christmas.jpg";
 
-export default function CustomizeMessage(){
-  return(
-    <div className="instructions">
-    <img src={Christmas} className="thumb" alt="Christmas images" />
-    
-    <p><strong>How to Reshare:</strong><br />Write your own custom message below</p>
-    </div>
-  )
+export default function CustomizeMessage({
+  email,
+  setEmail,
+  greeting,
+  setGreeting,
+  body,
+  setBody,
+  closing,
+  setClosing,
+  setPreview,
+  setCustomize,
+}) {
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, greeting, body, closing);
+    setPreview(true);
+    setCustomize(false);
+  };
+
+  return (
+    <>
+      <div className="instructions">
+        <img src={Christmas}  alt="Christmas image "  className="thumb"/>
+
+        <p>
+          <strong>How to Reshare:</strong>
+          <br />
+          Write your own custom message below
+        </p>
+      </div>
+
+      <form>
+        
+       
+        <div>
+        <label htmlFor="Email">Recipient Email </label><br />
+          <input
+            name="email"
+            type="text"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Recipient Email"
+          />
+        </div>
+   
+        <div>
+        <label htmlFor="Greeting">Greeting </label><br />
+          <input
+            name="greeting"
+            type="text"
+            value={greeting}
+            placeholder="Greeting"
+            onChange={(event) => setGreeting(event.target.value)}
+          />
+        </div>
+       
+        <div>
+        <label htmlFor="body">Body </label><br />
+          <textarea
+            name="Body"
+            type="textarea"
+            value={body}
+            onChange={(event) => setBody(event.target.value)}
+            placeholder="Body"
+            rows="3"
+          ></textarea>
+        </div>
+       
+        <div>
+        <label htmlFor="closing">Closing </label><br />
+          <input
+            name="closing"
+            type="text"
+            value={closing}
+            placeholder="Recipient Email"
+            onChange={(event) => setClosing(event.target.value)}
+          />
+
+        </div>
+        <button
+          type="submit"
+          onClick={
+            onSubmit
+          }
+        >
+          Preview Greeting Card
+        </button>
+      </form>
+    </>
+  );
 }
